1de07da5dda9812fbcaefe49aa94d117a74122f9
Kristof Hetzl <kristof.hetzl@gmail.com>
2021-07-13 15:30:41 +0100

    Fixing multiple custom-set-variables

--- custom.el
+++ custom.el
@@ -3,9 +3,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes '(tsdh-dark))
  '(package-selected-packages
-   '(exec-path-from-shell toml-mode company yasnippet flycheck lsp-ui lsp-mode rustic selectrum which-key use-package)))
+   '(exec-path-from-shell toml-mode company yasnippet flycheck lsp-ui lsp-mode rustic which-key use-package selectrum)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
